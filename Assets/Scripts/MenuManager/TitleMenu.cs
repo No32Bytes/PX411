8c4878887dafe2a91436c46f91897518fe226f33
No32Bytes <236882886+No32Bytes@users.noreply.github.com>
2025-12-27 21:40:57 +0100

    Restructure Unity MenuManager Hierachy
    Added connection from TitleMenu to SaveMenu and a Label to Display the currently selected save in the TitleMenu

--- Assets/Scripts/MenuManager/TitleMenu.cs
+++ Assets/Scripts/MenuManager/TitleMenu.cs
@@ -1,7 +1,14 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class TitleMenu : MonoBehaviour
 {
+    [SerializeField] private TMP_Text currentSaveIDLabel;
+    void OnEnable()
+    {
+        currentSaveIDLabel.SetText(GlobalDataStore.Instance.saveManager.CurrentSaveID);
+    }
+
     public void StartButtonOnClick()
     {
         GlobalDataStore.GetStateManager().menuManger.TitleMenuOpen = false;
