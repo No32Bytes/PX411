b50de4ab4cc4a1cc5fa0c4e0105ad5da14ca5504
marco <a@benni.de>
2026-04-22 18:16:08 +0200

    Teacher detect Player interaction

--- Assets/Scripts/Teacher/TeacherMovement.cs
+++ Assets/Scripts/Teacher/TeacherMovement.cs
@@ -8,28 +8,63 @@ public class TeacherMovement : MonoBehaviour
 {
     public Transform[] targets;
     public float speed = 5f;
+    public float rotationSpeed = 5f;
+
+    public Transform playerCamera;     
+    public float stopDistance = 5f;   
 
     private int currentTargetIndex = 0;
+    private bool isStopped = false;
 
     void Run()
     {
         if (targets.Length == 0) return;
 
-        Transform target = targets[currentTargetIndex];
+        
+        float distanceToPlayer = Vector3.Distance(transform.position, playerCamera.position);
 
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            target.position,
-            speed * Time.deltaTime
-        );
-
-        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        if (distanceToPlayer < stopDistance)
         {
-            currentTargetIndex++;
+            isStopped = true;
+        }
+        else
+        {
+            isStopped = false;
+        }
 
-            if (currentTargetIndex >= targets.Length)
+        if (isStopped)
+        {
+            
+            Vector3 dir = (playerCamera.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Transform target = targets[currentTargetIndex];
+
+         
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                target.position,
+                speed * Time.deltaTime
+            );
+
+            
+            Vector3 dir = (target.position - transform.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+
+       
+            if (Vector3.Distance(transform.position, target.position) < 0.1f)
             {
-                currentTargetIndex = 0;
+                currentTargetIndex++;
+
+                
+                if (currentTargetIndex >= targets.Length)
+                {
+                    currentTargetIndex = 0;
+                }
             }
         }
     }
