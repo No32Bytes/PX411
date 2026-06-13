c923550286d9152b5df81f43d0ec534c333e52c0
No32Bytes <236882886+No32Bytes@users.noreply.github.com>
2026-06-13 16:24:42 +0200

    Created prefab with will store the decorated school and made some doors in the school interactable

--- Assets/Scripts/Entity/DoorEntity.cs
+++ Assets/Scripts/Entity/DoorEntity.cs
@@ -7,45 +7,36 @@ public class DoorEntity : BaseEntity
     [SerializeField] private float targetRotationClose;
     [SerializeField] private float targetRotationOpen;
     [SerializeField] private float maxRotationChange = 180f;
+    [SerializeField] private bool rotateAroundZAxis = false;
+    [SerializeField] private bool antiClockWise = false;
     [SerializeField] private bool isOpen = false;
     private float targetRotation;
-    protected override void EntityAwake()
+    private void Awake()
+    {
+        entityId = "door";
+        usePhysics = false;
+    }
+    protected void Start()
     {
         float startRotation = isOpen ? targetRotationOpen : targetRotationClose;
-        transform.rotation = Quaternion.Euler(transform.rotation.x, startRotation, transform.rotation.z);
+
+        Vector3 angles = new(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z)
+        {
+            y = startRotation
+        };
+        transform.localEulerAngles = angles;
+        targetRotation = GetCurrentRotation();
     }
     public override void EntityInteraction()
     {
-        UpdateDoorState();
+        ToggleDoorState();
     }
-    protected void UpdateDoorState()
+    protected void ToggleDoorState()
     {
         if (isOpen)
             DoorClose();
         else
             DoorOpen();
-    }
-    private void Update()
-    {
-        float currenRotation = transform.rotation.eulerAngles.y;
-        if (currenRotation == targetRotation)
-            return;
-
-        float rotationChange = targetRotation - currenRotation;
-        if (Math.Abs(rotationChange) > maxRotationChange)
-            rotationChange = Mathf.Clamp(rotationChange, -maxRotationChange, maxRotationChange);
-
-        if (Math.Abs(rotationChange) < 1)
-        {
-            Vector3 setNextRotation = new(transform.rotation.x, targetRotation, transform.rotation.z);
-            transform.rotation = Quaternion.Euler(setNextRotation);
-            return;
-        }
-
-        rotationChange *= Time.deltaTime;
-        Vector3 nextRotation = new(0, rotationChange, 0);
-        transform.Rotate(nextRotation);
-
     }
     private void DoorOpen()
     {
@@ -57,5 +48,54 @@ public class DoorEntity : BaseEntity
     {
         targetRotation = targetRotationClose;
         isOpen = false;
+    }
+    private void Update()
+    {
+        float currenRotation = GetCurrentRotation();
+        if (currenRotation < 0)
+            currenRotation = 360 - currenRotation;
+        if (currenRotation == targetRotation)
+            return;
+
+        float rotationChange = targetRotation - currenRotation;
+        if (isOpen && rotationChange < 0)
+            rotationChange *= -1;
+        if (!isOpen && rotationChange > 0)
+            rotationChange *= -1;
+
+
+        if (antiClockWise)
+            rotationChange *= -1;
+
+
+
+
+        if (Math.Abs(rotationChange) > maxRotationChange)
+            rotationChange = Mathf.Clamp(rotationChange, -maxRotationChange, maxRotationChange);
+
+        if (Math.Abs(rotationChange) < 1)
+        {
+            Vector3 setNextRotation = new(transform.localEulerAngles.x, targetRotation, transform.localEulerAngles.z);
+            transform.localEulerAngles = setNextRotation;
+            return;
+        }
+
+        rotationChange *= Time.deltaTime;
+        Vector3 nextRotation = new(0, 0, 0);
+        SetRotationVector(ref nextRotation, rotationChange);
+        transform.Rotate(nextRotation);
+    }
+    private void SetRotationVector(ref Vector3 rotation, float value)
+    {
+        if (rotateAroundZAxis)
+        {
+            rotation.z = value;
+            return;
+        }
+        rotation.y = value;
+    }
+    private float GetCurrentRotation()
+    {
+        return transform.localEulerAngles.y;
     }
 }
