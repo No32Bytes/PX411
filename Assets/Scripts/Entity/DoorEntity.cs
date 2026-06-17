329dcbe86319434aa03a7b7b708d89298ea873d0
No32Bytes <236882886+No32Bytes@users.noreply.github.com>
2026-06-17 23:34:27 +0200

    Changed School Modell to fix Scale bug with doors

--- Assets/Scripts/Entity/DoorEntity.cs
+++ Assets/Scripts/Entity/DoorEntity.cs
@@ -1,16 +1,13 @@
+
 using System;
-using System.IO.Compression;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
 public class DoorEntity : BaseEntity
 {
     [SerializeField] private float targetRotationClose;
     [SerializeField] private float targetRotationOpen;
-    [SerializeField] private float maxRotationChange = 180f;
-    [SerializeField] private bool rotateAroundZAxis = false;
+    [SerializeField] private float rotationSpeed = 0.5f;
     [SerializeField] private bool antiClockWise = false;
-    [SerializeField] private bool useRotationZAxis = false;
     [SerializeField] private bool isOpen = false;
     [SerializeField] private BaseSoundEffect doorMovementSound;
     private AudioSource audioSource;
@@ -21,19 +18,53 @@ public class DoorEntity : BaseEntity
         entityId = "door";
         usePhysics = false;
     }
-    protected void Start()
+
+    private void Start()
     {
         float startRotation = isOpen ? targetRotationOpen : targetRotationClose;
-
-        Vector3 angles = new(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-        if (useRotationZAxis)
-            angles.z = startRotation;
-        else
-            angles.y = startRotation;
-
-        transform.localEulerAngles = angles;
-        targetRotation = GetCurrentRotation();
+        targetRotation = startRotation;
+        SetRotation(startRotation);
     }
+
+    private void Update()
+    {
+        float currenRotation = transform.eulerAngles.y;
+        if (currenRotation < 0)
+            currenRotation = 360 - currenRotation;
+
+        if (currenRotation == targetRotation)
+        {
+            audioSource.Stop();
+            return;
+        }
+
+        if (!audioSource.isPlaying)
+            AudioUtil.PlaySoundEffect(doorMovementSound, audioSource);
+
+        float rotationChangeMax = targetRotation - currenRotation;
+        if (isOpen && rotationChangeMax < 0)
+            rotationChangeMax *= -1;
+        if (!isOpen && rotationChangeMax > 0)
+            rotationChangeMax *= -1;
+
+        if(antiClockWise)
+            rotationChangeMax *= -1;
+
+        float rotationChange = rotationChangeMax * rotationSpeed * Time.deltaTime;
+
+        if (Math.Abs(rotationChange) > Math.Abs(rotationChangeMax))
+            rotationChange = rotationChangeMax;
+
+        SetRotation(currenRotation + rotationChange);
+    }
+
+    private void SetRotation(float rotation)
+    {
+        Vector3 vec = transform.rotation.eulerAngles;
+        vec.y = rotation;
+        transform.eulerAngles = vec;
+    }
+
     public override void EntityInteraction()
     {
         ToggleDoorState();
@@ -46,82 +77,15 @@ public class DoorEntity : BaseEntity
         else
             DoorOpen();
     }
-    private void DoorOpen()
+    public void DoorOpen()
     {
         targetRotation = targetRotationOpen;
         isOpen = true;
     }
 
-    private void DoorClose()
+    public void DoorClose()
     {
         targetRotation = targetRotationClose;
         isOpen = false;
-    }
-    private void Update()
-    {
-        float currenRotation = GetCurrentRotation();
-        if (currenRotation < 0)
-            currenRotation = 360 - currenRotation;
-        if (currenRotation == targetRotation)
-        {
-            audioSource.Stop();
-            return;
-        }
-        Debug.Log(currenRotation);
-
-        if (!audioSource.isPlaying)
-        {
-            AudioUtil.PlaySoundEffect(doorMovementSound, audioSource);
-        }
-
-        float rotationChange = targetRotation - currenRotation;
-        if (isOpen && rotationChange < 0)
-            rotationChange *= -1;
-        if (!isOpen && rotationChange > 0)
-            rotationChange *= -1;
-
-
-        if (antiClockWise)
-            rotationChange *= -1;
-
-
-
-
-        if (Math.Abs(rotationChange) > maxRotationChange)
-            rotationChange = Mathf.Clamp(rotationChange, -maxRotationChange, maxRotationChange);
-
-        if (Math.Abs(rotationChange) < 1)
-        {
-            Vector3 setNextRotation = new(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-
-            if (useRotationZAxis)
-                setNextRotation.z = targetRotation;
-            else
-                setNextRotation.y = targetRotation;
-
-            transform.localEulerAngles = setNextRotation;
-            return;
-        }
-
-        rotationChange *= Time.deltaTime;
-        Vector3 nextRotation = new(0, 0, 0);
-        SetRotationVector(ref nextRotation, rotationChange);
-        transform.Rotate(nextRotation);
-    }
-    private void SetRotationVector(ref Vector3 rotation, float value)
-    {
-        if (rotateAroundZAxis)
-        {
-            rotation.z = value;
-            return;
-        }
-        rotation.y = value;
-    }
-    private float GetCurrentRotation()
-    {
-        if (useRotationZAxis)
-            return transform.localEulerAngles.z;
-
-        return transform.localEulerAngles.y;
     }
 }
